dddc48dcde8c759bb98d5e071851a6a9c137f637
thebrettbarlow <me@brettbarlow.com>
2022-05-29 17:57:11 -0700

    about and contact pages

--- force-app/main/default/lwc/about/about.js
+++ force-app/main/default/lwc/about/about.js
@@ -1,3 +1,6 @@
 import { LightningElement } from 'lwc';
+import HEADSHOT from '@salesforce/resourceUrl/headshot';
 
-export default class About extends LightningElement {}
+export default class About extends LightningElement {
+  headshot = HEADSHOT;
+}
