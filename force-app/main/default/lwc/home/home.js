32d44f2ed312a513ea4cdec05b7f87125c9085c6
thebrettbarlow <me@brettbarlow.com>
2022-05-29 20:08:59 -0700

    adding pics from dreamforce

--- force-app/main/default/lwc/home/home.js
+++ force-app/main/default/lwc/home/home.js
@@ -1,3 +1,10 @@
 import { LightningElement } from 'lwc';
+import DREAMFORCE_2018_2 from '@salesforce/resourceUrl/dreamforce2018_2';
+import DREAMFORCE_2019_1 from '@salesforce/resourceUrl/dreamforce2019_1';
+import DREAMFORCE_2019_2 from '@salesforce/resourceUrl/dreamforce2019_2';
 
-export default class Home extends LightningElement {}
+export default class Home extends LightningElement {
+  dreamforce2018_2 = DREAMFORCE_2018_2;
+  dreamforce2019_1 = DREAMFORCE_2019_1;
+  dreamforce2019_2 = DREAMFORCE_2019_2;
+}
