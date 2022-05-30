c9b4fdbdbcc2d859a62329e446c9a364b60e9335
Brett Barlow <me@brettbarlow.com>
2022-05-30 11:40:08 -0700

    Home, Contact and About Pages for the Website (#11)
    * add favicon
    
    * about and contact pages
    
    * adding pics from dreamforce
    
    * home page for the website
    
    * make experience site live

--- force-app/main/default/lwc/home/home.js
+++ force-app/main/default/lwc/home/home.js
@@ -1,3 +1,10 @@
 import { LightningElement } from 'lwc';
+import DEV_USER_GROUP from '@salesforce/resourceUrl/devUserGroup';
+import DREAMFORCE_2018 from '@salesforce/resourceUrl/dreamforce2018';
+import DREAMFORCE_2019 from '@salesforce/resourceUrl/dreamforce2019';
 
-export default class Home extends LightningElement {}
+export default class Home extends LightningElement {
+  devUserGroup = DEV_USER_GROUP;
+  dreamforce2018 = DREAMFORCE_2018;
+  dreamforce2019 = DREAMFORCE_2019;
+}
