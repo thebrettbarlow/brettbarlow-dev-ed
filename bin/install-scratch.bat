7c4b0c75ec2401c316835216e41a16e5e9b5773d
thebrettbarlow <me@brettbarlow.com>
2022-05-30 16:46:39 -0700

    update windows install script

--- bin/install-scratch.bat
+++ bin/install-scratch.bat
@@ -1,49 +1,80 @@
 @echo OFF
 
 rem Set parameters
-set ORG_ALIAS=az-insurance
+set ORG_ALIAS=brettbarlow-scratch
 
 @echo:
-echo Installing AZ Insurance scratch org (%ORG_ALIAS%)
+echo Installing scratch org (%ORG_ALIAS%)
 @echo:
 
 rem Install script
 echo Cleaning previous scratch org...
-cmd.exe /c sfdx force:org:delete -p -u %ORG_ALIAS% 2>NUL
+cmd.exe /c sfdx force:org:delete ^
+  --noprompt ^
+  --targetusername=%ORG_ALIAS% 2>NUL
 @echo:
 
 echo Creating scratch org...
-cmd.exe /c sfdx force:org:create -s -f config/project-scratch-def.json -d 30 -a %ORG_ALIAS%
+cmd.exe /c sfdx force:org:create ^
+  --definitionfile=config/project-scratch-def.json ^
+  --setalias=%ORG_ALIAS% ^
+  --setdefaultusername
 call :checkForError
 @echo:
 
-echo Creating dummy Experience site...
-cmd.exe /c sfdx force:community:create --name "Dummy" --templatename "Aloha" -p "dummy"
+echo Creating Experience site...
+cmd.exe /c sfdx force:community:create ^
+  --targetusername=%ORG_ALIAS% ^
+  --name=Home ^
+  --urlpathprefix="" ^
+  --templatename="Build Your Own (LWR)" ^
+  templateParams.AuthenticationType=UNAUTHENTICATED
 call :checkForError
 @echo:
 
-echo Deploying standard metadata...
-cmd.exe /c sfdx force:source:deploy -m ApexClass,Layout,CustomObject,LightningComponentBundle,ManagedContentType,CustomObject,StaticResource,CustomTab,PermissionSet,Flow
+echo Sleeping 30s for Experience site deployment
+timeout 30 > NUL
+@echo:
+
+echo Deploying base metadata...
+cmd.exe /c sfdx force:source:deploy ^
+  --targetusername=%ORG_ALIAS% ^
+  --metadata=AuraDefinitionBundle,ApexClass,Flexipage,Layout,LightningComponentBundle,CustomObject,PermissionSet,StaticResource,CustomTab
+call :checkForError
+@echo:
+
+echo Deploying Experience site metadata...
+cmd.exe /c sfdx force:source:deploy ^
+  --targetusername=%ORG_ALIAS% ^
+  --metadata=ExperienceBundle,NavigationMenu,Network,ApexPage,Profile,CustomSite
+call :checkForError
+@echo:
+
+echo Publishing Experience site...
+cmd.exe /c sfdx force:community:publish ^
+  --targetusername=%ORG_ALIAS% ^
+  --name=Home
+call :checkForError
+@echo:
+
+echo Assigning Permission Set...
+cmd.exe /c sfdx force:user:permset:assign ^
+  --targetusername=%ORG_ALIAS% ^
+  --permsetname=Projects_User
 call :checkForError
 @echo:
 cd %CD%/..
 
-echo Deploying Experience site metadata...
-cmd.exe /c sfdx force:source:deploy -m ApexPage,CustomSite,ExperienceBundle,NavigationMenu,Network,Profile
+echo Importing sample data...
+cmd.exe /c sfdx force:data:tree:import ^
+  --targetusername=%ORG_ALIAS% ^
+  --plan=data/Project__c-plan.json
 call :checkForError
 @echo:
 
-echo Assigning permission set for Marketing Site Builder
-cmd.exe /c sfdx force:user:permset:assign -n LWR_Marketing_Builder
-@echo:
-
-echo Publishing Marketing Site...
-cmd.exe /c sfdx force:community:publish -n "LWR Demo Marketing" 
-call :checkForError
-@echo:
-
-echo Publishing Agent Portal...
-cmd.exe /c sfdx force:community:publish -n "LWR Demo Agent" 
+echo Opening org...
+cmd.exe /c sfdx force:org:open ^
+  --targetusername=%ORG_ALIAS%
 call :checkForError
 @echo:
 
