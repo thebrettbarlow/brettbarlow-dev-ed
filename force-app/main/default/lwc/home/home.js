67e20b83e2fc1ae268ccf9d65cedb5731624b126
thebrettbarlow <me@brettbarlow.com>
2022-06-29 21:19:26 -0700

    move images to folder and remove unnecessary xml files

--- force-app/main/default/lwc/home/home.js
+++ force-app/main/default/lwc/home/home.js
@@ -1,10 +1,8 @@
 import { LightningElement } from 'lwc';
-import DEV_USER_GROUP from '@salesforce/resourceUrl/devUserGroup';
-import DREAMFORCE_2018 from '@salesforce/resourceUrl/dreamforce2018';
-import DREAMFORCE_2019 from '@salesforce/resourceUrl/dreamforce2019';
+import WEBSITE_ASSETS from '@salesforce/resourceUrl/website';
 
 export default class Home extends LightningElement {
-  devUserGroup = DEV_USER_GROUP;
-  dreamforce2018 = DREAMFORCE_2018;
-  dreamforce2019 = DREAMFORCE_2019;
+  devUserGroup = `${WEBSITE_ASSETS}/dreamforce-2018-2.jpg`;
+  dreamforce2018 = `${WEBSITE_ASSETS}/dreamforce-2018.jpg`;
+  dreamforce2019 = `${WEBSITE_ASSETS}/dreamforce-2019.jpg`;
 }
