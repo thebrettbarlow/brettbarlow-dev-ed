1b2f8a1fcf74272b4b020f2fd000ef5cff27792b
thebrettbarlow <me@brettbarlow.com>
2022-07-05 15:31:02 -0700

    add spinner until resume data loads

--- force-app/main/default/lwc/resume/resume.js
+++ force-app/main/default/lwc/resume/resume.js
@@ -39,6 +39,10 @@ export default class Resume extends LightningElement {
   })
   resume;
 
+  get isResumeLoaded() {
+    return this.resume;
+  }
+
   @wire(getUniqueSkillsFromResumeEntries, { resumeId: '$recordId' })
   skills;
 
